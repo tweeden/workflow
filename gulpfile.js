3dc0f3dc593e37516d38c9a541b2648dc43bcf55
tweeden <terrenceweeden@group360.com>
2017-01-02 11:35:19 -0600

    Added watch to default task

--- gulpfile.js
+++ gulpfile.js
@@ -43,9 +43,10 @@ gulp.task('compass', function() {
 gulp.task('watch', function() {
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
+	gulp.watch('components/sass/*.scss', ['compass']);
 });
 
-gulp.task('default', ['coffee', 'js', 'compass']);
+gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
 
 
 
